e81c6fbdc852250b5c40d5452028e8c69cbb28ba
birdman7010 <mansfield8@yahoo.com>
2015-11-10 20:03:31 -0700

    Added Project File

---
+++ app/src/androidTest/java/com/bufflife/bufflife/busTrackerTest.java
@@ -0,0 +1,27 @@
+package com.bufflife.bufflife;
+
+/**
+ * Created by birdman on 11/7/15.
+ */
+
+import com.bufflife.bufflife.busTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+
+public class busTrackerTest {
+    @Test
+    public void clickingbustracker1_shouldStartLoginActivity() {
+        busTracker activity = Robolectric.setupActivity(busTracker.class);
+        activity.findViewById(R.id.bustracker1).performClick();
+
+        Intent expectedIntent = new Intent(activity, busTracker.class);
+        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+}
