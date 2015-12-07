8113fe1f70e7ff9754e97367776aa394dd7822c2
Kyle Knight <knightkd@colorado.edu>
2015-12-07 01:52:27 -0700

    touched up comments and did some javadoc prep

--- app/src/main/java/com/bufflife/bufflife/diningHallMenu.java
+++ app/src/main/java/com/bufflife/bufflife/diningHallMenu.java
@@ -17,17 +17,15 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Formatter;
-import java.util.Locale;
 
 /**
-<<<<<<< Updated upstream
- * @author Created by danielyedidovich on 11/11/15.
- * @author last edited by Kyle Knight 11/18/2015  had to change to async task to work
- * We could also simplify to only show meals based on time of day but I think it makes more sense to see the whole week.
- * Also my I'm only getting a saturday value for the calendar date and it is definitely not saturday...
-=======
- * @author Created by danielyedidovich
->>>>>>> Stashed changes
+ * <H1> view sewall and libby dining halls daily menu</H1>
+ * This class opens the sewall and libby dining hall menu online a scrapes them using jsoup. Then it posts the menu available in each hall for the current day.
+ * @author Daniel Yedidovich
+ * @author edited by Kyle Knight 11/18/2015
+ * @since 11/11/2015
+ * had to change to async task to make it work
+ *
  */
 public class diningHallMenu extends Activity{
     //menu URLs
@@ -38,7 +36,9 @@ public class diningHallMenu extends Activity{
 
     TextView sewallTextView;
     TextView libbyTextView;
-
+/**
+ * This method that calls the background methods when this piece of the app is opened. While waiting to display the menu it displays a basic loading screen.
+ */
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -49,7 +49,7 @@ public class diningHallMenu extends Activity{
 
         ( new diningHallMenuBackground() ).execute(new String[]{sewallDiningWeb,"sewall"});
 
-        ( new diningHallMenuBackground() ).execute(new String[]{libbyDiningWeb,"libby"});
+        (new diningHallMenuBackground() ).execute(new String[]{libbyDiningWeb,"libby"});
         sewallTextView.setText("Loading...");
         libbyTextView.setText("Loading...");
 
@@ -57,22 +57,31 @@ public class diningHallMenu extends Activity{
 
 
     /**
-     * @author Created by Kyle on 11/12/2015.
+     * <H1> Background Website Scraper</H1>
+     * This class runs the jsoup scraping on a separate string so the the phone OS doesn't kill the thread for taking too long to respond.
+     * @author Created by Kyle on 11/12/2015
      */
     public class diningHallMenuBackground extends AsyncTask<String,Void,String> {
-        String title;
-        String carrier;
+        String currentMenu;
+        String selectedHall;
         Calendar calendar = Calendar.getInstance();
         StringBuilder sb = new StringBuilder();
         Formatter formatter = new Formatter();
+
+        /**
+         *
+         * @param whichHall a single string that had to be passed as an array of strings due to inheritance.
+         * @return currentMenu returns the parsed menu data to be displayed on screen depending on the day of the week.
+         */
         @Override
-        public String doInBackground(String... strings){
-            carrier=strings[1];
+        public String doInBackground(String... whichHall){
+
+            selectedHall=whichHall[1];
             Log.d("isCON", "Connecting");
             Document doc = null;
             //pull down and parse the menu HTML
             try {
-                doc = Jsoup.connect(strings[0]).get();
+                doc = Jsoup.connect(whichHall[0]).get();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -80,116 +89,120 @@ public class diningHallMenu extends Activity{
             Elements links = null;
             //Find the items that match the tags for the day of the week and the specific meal
             if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.MONDAY) {
-                title = "Breakfast: \n";
+                currentMenu = "Breakfast: \n";
                 links = doc.select("tr#breakfastRow td.mon li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nLunch: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.mon li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.mon li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.TUESDAY) {
-                title = "Breakfast: \n";
+                currentMenu = "Breakfast: \n";
                 links = doc.select("tr#breakfastRow td.tues li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nLunch: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.tues li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
                 links = doc.select("tr#lunchRow td.'tues here' li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.tues li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.WEDNESDAY) {
-                title = "Breakfast: \n";
+                currentMenu = "Breakfast: \n";
                 links = doc.select("tr#breakfastRow td.wed li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nLunch: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.wed li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.wed li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.THURSDAY) {
-                title = "Breakfast: \n";
+                currentMenu = "Breakfast: \n";
                 links = doc.select("tr#breakfastRow td.thurs li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nLunch: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.thurs li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.thurs li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.FRIDAY) {
-                title = "Breakfast: \n\n";
+                currentMenu = "Breakfast: \n\n";
                 links = doc.select("tr#breakfastRow td.fri li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nLunch: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.fri li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.fri li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.SATURDAY) {
-                title ="Breakfast: \nNo Meal Served\n\nLunch: \n";
+                currentMenu ="Breakfast: \nNo Meal Served\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.sat li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.sat li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else if(calendar.get(Calendar.DAY_OF_WEEK) == calendar.SUNDAY) {
-                title ="Breakfast: No Meal Served\n\nLunch: \n";
+                currentMenu ="Breakfast: No Meal Served\n\nLunch: \n";
                 links = doc.select("tr#lunchRow td.sun li");
                 for (Element element: links)
-                    title += element.text() + "\n";
-                title += "\n\nDinner: \n";
+                    currentMenu += element.text() + "\n";
+                currentMenu += "\n\nDinner: \n";
                 links = doc.select("tr#dinnerRow td.sun li");
                 for (Element element: links)
-                    title += element.text() + "\n";
+                    currentMenu += element.text() + "\n";
             }
             else {
-                title ="Could not determine menu";
+                currentMenu ="Could not determine menu";
             }
-            Log.d("Menu practice", "Breakfast [" + title + "]");
-            Log.d("Menu practice", "Lunch [" + title + "]");
-            Log.d("Menu practice", "Dinner [" + title + "]");
+            Log.d("Menu practice", "Breakfast [" + currentMenu + "]");
+            Log.d("Menu practice", "Lunch [" + currentMenu + "]");
+            Log.d("Menu practice", "Dinner [" + currentMenu + "]");
 
 
-            return title;
+            return currentMenu;
         }
 
+        /**
+         * This method posts the returned menu to the screen
+         * @param s included to please the inherited method
+         */
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            if (carrier=="sewall")
-                sewallTextView.setText(title);
+            if (selectedHall =="sewall")
+                sewallTextView.setText(currentMenu);
             else
-                libbyTextView.setText(title);
+                libbyTextView.setText(currentMenu);
         }
     }
 
